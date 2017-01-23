a0dea63fb483b438391eef51fa8ff8ef35dd170e
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2017-01-23 18:00:54 +0200

    Add placeholder music.

--- run.bat
+++ run.bat
@@ -7,9 +7,6 @@ rgbasm -o header.o header.z80
 rgbasm -o skrollidemo.o skrollidemo.z80
 @if errorlevel 1 goto error
 
-REM rgbasm -o fades.o rgbgrafx/FADES.z80
-REM @if errorlevel 1 goto error
-
 rgbasm -i rgbgrafx/ -o maps.o rgbgrafx/maps.z80
 @if errorlevel 1 goto error
 
@@ -19,16 +16,22 @@ rgbasm -i rgbgrafx/ -o pals.o rgbgrafx/pals.z80
 rgbasm -i rgbgrafx/ -o rgbgrafx.o rgbgrafx/rgbgrafx.z80
 @if errorlevel 1 goto error
 
-REM rgbasm -o sprites.o sprites.z80
-REM @if errorlevel 1 goto error
-
 rgbasm -i rgbgrafx/ -o tiles.o rgbgrafx/tiles.z80
 @if errorlevel 1 goto error
+
+rgbasm -o gbt_player.o gbt_player.asm
+@if errorlevel 1 goto error
+
+rgbasm -o gbt_player_bank1.o gbt_player_bank1.asm
+@if errorlevel 1 goto error
+
+rgbasm -o skrolli_anthem.o skrolli_anthem.asm
 
 
 @echo Linking...
 rgblink -n skrollidemo.sym -m skrollidemo.map -o skrollidemo.gb *.o
 @if errorlevel 1 goto error
+
 
 @echo Fixing...
 rgbfix -p0 -v skrollidemo.gb
