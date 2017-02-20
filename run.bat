4d26a17fb6d70daf15365b08a6f09604e988caf4
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2017-02-20 16:35:11 +0200

    Move logo code in separate file.

--- run.bat
+++ run.bat
@@ -23,6 +23,9 @@ rgbasm.exe -o build/header.o header.z80
 rgbasm.exe -o build/skrollidemo.o skrollidemo.z80
 @if errorlevel 1 goto error
 
+rgbasm.exe -o build/scene_skrolli_logo.o scene_skrolli_logo.z80
+@if errorlevel 1 goto error
+
 rgbasm.exe -i rgbgrafx/ -o build/maps.o rgbgrafx/maps.z80
 @if errorlevel 1 goto error
 
